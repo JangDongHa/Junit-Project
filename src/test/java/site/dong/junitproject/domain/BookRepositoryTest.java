6bac0774fb143cc690f6fc2c2cc64ee096e77a88
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-08-04 13:09:19 +0900

    Repository 테스트 환경 설정 완료

--- src/test/java/site/dong/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/dong/junitproject/domain/BookRepositoryTest.java
@@ -1,4 +1,29 @@
 package site.dong.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩 (= 컨트롤러와 서비스는 메모리에 뜨지 않음)
 public class BookRepositoryTest {
+
+    @Autowired // DI
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    public void bookSaveTest(){
+        System.out.println("ㅎㅇㅎㅇ");
+    }
+
+    // 2. 책 목록 보기
+
+
+    // 3. 책 1건 보기
+
+
+    // 4. 책 수정
+
+
+    // 5. 책 삭제
 }
