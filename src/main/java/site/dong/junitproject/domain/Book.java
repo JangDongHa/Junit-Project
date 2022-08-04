14214363949a02e9cad87b13f22aaad367efb9cb
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-08-04 12:49:06 +0900

    Book Entity 생성 완료

--- src/main/java/site/dong/junitproject/domain/Book.java
+++ src/main/java/site/dong/junitproject/domain/Book.java
@@ -1,4 +1,26 @@
 package site.dong.junitproject.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 20)
+    private String author;
 }
